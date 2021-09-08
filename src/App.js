025bcdccebc2ef9aec141d8f76b0d4270ba67a61
Mark Atherton <markatherton@talkholiday.com>
2021-09-08 12:46:12 +0100

    added store/auth-context.js and added Provider on App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+import AuthContext from './store/auth-content';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -28,13 +30,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
