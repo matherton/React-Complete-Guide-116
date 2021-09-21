cc9c2b54322b2a1970fb4f73ee17e6f8a153ea1a
Mark Atherton <mark@insightinternet.co.uk>
2021-09-21 12:18:46 +0100

    added useRef and useEffect hooks to focus on first invalid input using ref prop on native input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import classes from './Input.module.css';
 
 const Input = (props) => {
+  const inputRef = useRef();
+
+  useEffect( () => {
+    inputRef.current.focus()
+  },[]);
+  
   return (
     <div
       className={`${classes.control} ${
@@ -11,6 +17,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
